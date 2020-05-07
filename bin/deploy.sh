207cbfea9598ffadf55df281484560d850176f56
wenju xing <linuxing3@qq.com>
2020-05-06 20:13:03 -0400

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,17 +1,24 @@
+#install hugo
+wget -O hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.70.0/hugo_0.70.0_Linux-64bit.deb 
+sudo dpkg -i hugo.deb
+type hugo
+
+# create site
+hugo new site docs/site
+cp docs/*.org docs/site/content/
+
+# install them
+cd docs/site
+git init
+git submodule add https://github.com/budparr/gohugo-theme-ananke.git themes/ananke
+echo 'theme = "ananke"' >> config.toml
+sed -i 's/example\.org/xunqinji\.top\/hugo/g' config.toml
+echo "Done with settings!"
+
 # build
-cd ~/docs
 hugo -D
 
-
-# make key
-mkdir ~/.ssh
-cat > ~/.ssh/id_rsa << EOF
-$DEPLOY_KEY
-EOF
-chmod 600 ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa
-
 # deploy
-cd ~/docs
-echo "yes
-"|bash <(rsync -avzr --delete --exclude .git/ public/ $USER@$HOST:$DIR)
+#cd ~/docs
+#echo "yes
+#"|bash <(rsync -avzr --delete --exclude .git/ public/ $USER@$HOST:$DIR)
