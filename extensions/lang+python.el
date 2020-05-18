ed15801a20f2f040daa797bfd882ddc04b4fa0fe
linuxing3 <linuxing3@qq.com>
2020-05-18 16:13:30 -0400

    change windows font

--- extensions/lang+python.el
+++ extensions/lang+python.el
@@ -1,5 +1,35 @@
 ;;; c:/Users/Administrator/.doom.d/lang+python.el -*- lexical-binding: t; -*-
+;;;
+;;;
+;;; FIXME ~/.emacs.d/modules/lang/python.config
+;;; Enable anaconda
+;;;
+(use-package! conda
+  :when (featurep! +conda)
+  :after python
+  :config
+  ;; The location of your anaconda home will be guessed from a list of common
+  ;; possibilities, starting with `conda-anaconda-home''s default value (which
+  ;; will consult a ANACONDA_HOME envvar, if it exists).
+  ;;
+  ;; If none of these work for you, `conda-anaconda-home' must be set
+  ;; explicitly. Afterwards, run M-x `conda-env-activate' to switch between
+  ;; environments
+  (or (cl-loop for dir in (list conda-anaconda-home
+                                "d:/var/anaconda3"
+                                "d:/lib/anaconda3"
+                                "d:/var/anaconda2"
+                                "~/.anaconda"
+                                "~/.miniconda"
+                                "~/.miniconda3"
+                                "~/miniconda3"
+                                "/usr/bin/anaconda3"
+                                "/usr/local/anaconda3"
+                                "/usr/local/miniconda3"
+                                "/usr/local/Caskroom/miniconda/base")
+               if (file-directory-p dir)
+               return (setq conda-anaconda-home dir
+                            conda-env-home-directory dir))
+      (message "Cannot find Anaconda installation"))
 
-(setq conda-anaconda-home "D:/lib/anaconda3")
-(setq conda-env-home-directory "D:/lib/anaconda3")
-(setq conda-env-autoactivate-mode t)
+  (setq conda-env-autoactivate-mode t))
