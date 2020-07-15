839b78d2045b5c4724fa234846916932fe63b07e
linuxing3 <linuxing3@qq.com>
2020-07-15 19:53:16 -0400

    delete server file

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -28,7 +28,6 @@
  ;; Spacemacs to other dir
  ("spacemacs" . ((env . (("HOME" . "D:/Users/root")
                          ("EMACSDIR" . "D:/Users/root/.emacs.d")
-                         ("EMACS_SERVER_FILE" . "D:/Users/root/.emacs.d/.cache/server/server")
                          ("SPACEMACSDIR" . "D:/Users/root/.spacemacs.d")))
                  (server-name . "D:/Users/root/.emacs.d/.cache/server/server")
                  (user-emacs-directory . "D:/Users/root/.emacs.d")))
@@ -36,19 +35,15 @@
  ;; Doom to C drive with default home dir
  ("doom" . ((env . (("HOME" . "C:/Users/Wjb")
                     ("EMACSDIR" . "C:/Users/Wjb/.emacs.d")
-                    ("EMACS_SERVER_FILE" . "c:/Users/Wjb/.emacs.d/.local/cache/server/server")
                     ("DOOMDIR" . "C:/Users/Wjb/.doom.d")))
             (user-emacs-directory . "C:/Users/Wjb/.emacs.d")
-            (server-name . "c:/Users/Wjb/.emacs.d/.local/cache/server/server")
             (doom-private-dir . "C:/Users/wjb/.doom.d")))
 
  ;; Simplified Doom to D drive with customized home dir
  ("cleandoom" . (
                  (env . (("HOME" . "D:/Users/xingwenju")
                          ("EMACSDIR" . "D:/Users/xingwenju/.emacs.d")
-                         ("EMACS_SERVER_FILE" . "D:/Users/xingwenju/.emacs.d/.local/cache/server/server")
                          ("DOOMDIR" . "D:/Users/xingwenju/.doom.d")))
                  (user-emacs-directory . "D:/Users/xingwenju/.emacs.d")
-                 (server-name . "D:/Users/xingwenju/.emacs.d/.local/cache/server/server")
                  (doom-private-dir . "D:/Users/xingwenju/.doom.d")))
  )
